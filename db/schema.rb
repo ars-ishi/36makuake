b185831b590bdde5eaa5e265ad5cb1846869c0bd
ars-ishi <ars.ishi11@gmail.com>
2018-10-12 20:05:18 +0900

    Add category model, table
    categoryのモデルとテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181012105309) do
+ActiveRecord::Schema.define(version: 20181012110137) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "payment_methods", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id", null: false
