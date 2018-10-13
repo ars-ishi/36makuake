5bc68a0bd37daea3428fd93cc1744d06e868a804
ars-ishi <ars.ishi11@gmail.com>
2018-10-13 14:44:18 +0900

    Edit user model,  define association
    Userモデルにアソシエーションを追加しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one  :promoter_profile, dependent: :destroy
+  has_many :send_addresses, dependent: :destroy
+  has_one  :payment_method
+  has_many :project_likes, dependent: :destroy
+  has_many :projects, through: :project_likes
+  has_many :direct_messages, dependent: :destroy
+  has_many :projects, through: :direct_messages
+  has_many :orders, dependent: :destroy
+  has_many :report_likes, dependent: :destroy
+  has_many :report, through: :report_likes
 end
