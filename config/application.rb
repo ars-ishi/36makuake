cd3f391207d2b1e649e72093d55309fee93c6935
ars-ishi <ars.ishi11@gmail.com>
2018-10-15 17:45:47 +0900

    Devise message translate
    Deviseのメッセージを日本語化しました。

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Makuake
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.default_locale = :ja
     config.load_defaults 5.1
     config.generators do |g|
       g.stylesheets false
