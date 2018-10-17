82967d951459f4e1621ed3769173737dc9b5be41
ars-ishi <ars.ishi11@gmail.com>
2018-10-17 18:43:01 +0900

    Edit config/routes.rb ( resolve conflict )
    ・コンフリクト解消のために編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'projects#index'
-  resources :users, only: [:show, :edit, :update]
+  resources :users do
+    resources :orders, only: [:new, :create]
+  end
   get 'users/:id/edit_omniauth' => 'users#edit_omniauth'
   patch 'users/:id/update_omniauth' => 'users#update_omniauth'
 end
