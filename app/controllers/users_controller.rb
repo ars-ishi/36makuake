616b8dfb83266b482922cead46ed7fa692b34194
ars-ishi <ars.ishi11@gmail.com>
2018-10-17 20:22:23 +0900

    Edit users_controller ( change argument from URI to prefix)
    redirect_toの引数をprefixで指定するようにしました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     @name = omniauth_params[:name]
     user = current_user
     unless user.update( name: omniauth_params[:name], email: omniauth_params[:email] )
-      redirect_to "/users/#{current_user.id}/edit_omniauth", alert: 'メールアドレスがすでに使用されています。'
+      redirect_to user_edit_omniauth_path(current_user), alert: 'メールアドレスがすでに使用されています。'
     end
   end
 
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   def move_to_email_registration
     if user_signed_in?
       if current_user.email.include?("-facebook@example.com") ||  current_user.email.include?("-twitter@example.com")
-        redirect_to "/users/#{current_user.id}/edit_omniauth", alert: '情報の登録をお願いします'
+        redirect_to user_edit_omniauth_path(current_user), alert: '情報の登録をお願いします'
       end
     end
   end
