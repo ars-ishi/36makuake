01345a377da47c8ceaf00ff27e432f04ccdd94a5
ars-ishi <ars.ishi11@gmail.com>
2018-10-17 20:17:10 +0900

    Edit models/user.rb ( from "where" to "find_by" )
    ユーザーを検索するのに.whereと.firstにしていた部分を、.find_byに変更しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   has_many :report, through: :report_likes
 
   def self.find_for_oauth(auth)
-    user = User.where(uid: auth.uid, provider: auth.provider).first
+    user = User.find_by(uid: auth.uid, provider: auth.provider)
 
     unless user
       user = User.create(
