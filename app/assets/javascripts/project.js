87c4387a001e65158740875dcb37f8a9c738ca8c
MCUSK <mcuskg@gmail.com>
2018-10-18 18:56:26 +0900

    fade Page-top

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,5 +1,7 @@
 $(function() {
 
+
+  //ボックスの表示
   $(".link03After").on("click",function(){
    if($("#myMenuBox").css("display") == "none"){
     $("#toolTipsPjt").css("display","none");
@@ -10,8 +12,6 @@ $(function() {
     console.log("2")
    }
   })
-
-
     $(".header-bell-after").on("click",function(){
    if($("#toolTipsPjt").css("display") == "none"){
     $("#myMenuBox").css("display","none");
@@ -24,16 +24,40 @@ $(function() {
   })
 
 
+  //page-topボタン
+$(function() {
+    var topBtn = $('#page-top_jq');
+    topBtn.hide();
+    //スクロールが100に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+        //ボタンの表示方法
+            topBtn.fadeIn();
+        } else {
+        //ボタンの非表示方法
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,  html').animate({
+            scrollTop: 0
+        },   500);
+        return false;
+    });
+});
+
+
   // スライダー
   $(document).ready(function () {
     //initialize swiper when document ready
           var swiper = new Swiper('.swiper-container', {
             speed: 400,
             loop: true,
-            // autoplay: {
-            //   delay: 7500,
-            //   disableOnInteraction: false,
-            // },
+            autoplay: {
+              delay: 7500,
+              disableOnInteraction: false,
+            },
             pagination: {
               el: '.swiper-pagination',
               type: 'bullets',
