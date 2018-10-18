ecdb47167c4dd6ea95087f6df629443026bcad75
MCUSK <mcuskg@gmail.com>
2018-10-18 18:46:50 +0900

    ボックスの表示

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,4 +1,30 @@
 $(function() {
+
+  $(".link03After").on("click",function(){
+   if($("#myMenuBox").css("display") == "none"){
+    $("#toolTipsPjt").css("display","none");
+    $("#myMenuBox").css("display","block");
+    console.log("1")
+   } else{
+    $("#myMenuBox").css("display","none");
+    console.log("2")
+   }
+  })
+
+
+    $(".header-bell-after").on("click",function(){
+   if($("#toolTipsPjt").css("display") == "none"){
+    $("#myMenuBox").css("display","none");
+    $("#toolTipsPjt").css("display","block");
+    console.log("1")
+   } else{
+    $("#toolTipsPjt").css("display","none");
+    console.log("2")
+   }
+  })
+
+
+  // スライダー
   $(document).ready(function () {
     //initialize swiper when document ready
           var swiper = new Swiper('.swiper-container', {
