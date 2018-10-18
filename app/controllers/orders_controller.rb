f9607aa88fc5b92fbd1e113b297214767774ebd3
masuyu <masujazzcat@gmail.com>
2018-10-18 18:01:01 +0900

    orders_controller.rb
    Newメソッドにインスタンス変数を記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,34 @@
 class OrdersController < ApplicationController
   def new
+    @order = Order.new
+
+    @user = User.find(current_user.id)
+    @usersAddresses = @user.send_addresses
+
+    # メインのアドレスを抽出するメソッド
+    array = @user.send_addresses.where(main: "1")
+    @address = {}
+    array.each do |ele|
+      @address = ele
+    end
+
+    @course = Course.find(course_params[:course_id])
   end
 
   def create
+  end
+
+private
+  def course_params
+    params.permit(:project_id, :course_id)
+  end
+
+  def order_params
+  end
+
+  def order_detail_params
+  end
+
+  def order_answer_params
   end
 end
