f8735113d9126c931b00e34e6707521ce25be427
mitsuyoshi <m123matsuo@icloud.com>
2018-10-19 19:12:40 +0900

    Create_users_controller.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def invest
+  end
+
+  def message
+  end
+
+  def popup
+  end
+
+  def destroy
+  end
+
+end
