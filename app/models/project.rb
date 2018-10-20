20b45ecc26ff300a9e03c2b26dff1d282fddbc87
MCUSK <mcuskg@gmail.com>
2018-10-20 15:35:32 +0900

    Bug fix

--- app/models/project.rb
+++ app/models/project.rb
@@ -31,7 +31,7 @@ class Project < ApplicationRecord
       return "#{days} 日"
     elsif  hour > 0 && days == 0
       return "#{hour} 時間"
-    elsif minute > 0 && days == 0
+    elsif minute > 0 && days == 0 && hour == 0
       return "#{minute-1} 分"
     else
       return "終了"
