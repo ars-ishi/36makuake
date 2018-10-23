fc9297e835812a5e59cc82ae2f136c3d8b2c5faa
MCUSK <mcuskg@gmail.com>
2018-10-23 14:22:42 +0900

    カテゴリーの反映

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     collection{ get "search" }
   end
 
-  resources :discovers, only: [:index, :new, :show] do
+  resources :discovers, only: [:index, :new] do
     collection do
       get :soon
       get :pickup
