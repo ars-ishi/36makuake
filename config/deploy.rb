09e43d4b6f8cf2fc97176c432121c8d62d74a807
masuyu <masujazzcat@gmail.com>
2018-10-23 16:33:59 +0900

    修正config/deploy.rb
    taskを追記

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,7 +51,16 @@ set :keep_releases, 5
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
+  task :start do
+    run "bundle exec unicorn_rails -c #{current_path}/config/unicorn.rb -E production -D"
+  end
+
+  task :stop do
+    run "kill -QUIT `cat #{current_path}/tmp/pids/unicorn.pid`"
+  end
+
   task :restart do
-    invoke 'unicorn:restart'
+    run "kill -USR2 `cat #{current_path}/tmp/pids/unicorn.pid`"
+    run "kill -QUIT `cat #{current_path}/tmp/pids/unicorn.pid.oldbin`"
   end
 end
