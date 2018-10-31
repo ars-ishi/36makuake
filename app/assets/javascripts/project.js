3224f2b8ef16a7e8d49bb272d681185613abbe6e
ars-ishi <ars.ishi11@gmail.com>
2018-10-31 21:00:29 +0900

    Edit javascripts/project.js
    ヘッダーのボタンが効かないエラーを修正しました。

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,67 +1,63 @@
 $(document).on('turbolinks:load', function() {
-
-$('.modal').modaal();
-
+  $('.modal').modaal();
+});
+$(document).on('turbolinks:load', function() {
   //ヘッダーのマイページボックスの表示
-  $(".link03After").on("click",function(){
-   if($("#myMenuBox").css("display") == "none"){
+  $(".link03After").on("click",function() {
+    if ($("#myMenuBox").css("display") == "none") {
+      $("#toolTipsPjt").css("display","none");
+      $("#myMenuBox").css("display","block");
+    } else {
+      $("#myMenuBox").css("display","none");
+    }
+  });
+  $(".header-bell-after").on("click",function() {
+    if ($("#toolTipsPjt").css("display") == "none") {
+      $("#myMenuBox").css("display","none");
+      $("#toolTipsPjt").css("display","block");
+    } else {
+      $("#toolTipsPjt").css("display","none");
+    }
+  });
+  $("#wrapper").on("click",function() {
     $("#toolTipsPjt").css("display","none");
-    $("#myMenuBox").css("display","block");
-   } else{
     $("#myMenuBox").css("display","none");
-   }
-  })
-    $(".header-bell-after").on("click",function(){
-   if($("#toolTipsPjt").css("display") == "none"){
-    $("#myMenuBox").css("display","none");
-    $("#toolTipsPjt").css("display","block");
-   } else{
-    $("#toolTipsPjt").css("display","none");
-   }
-  })
-  $("#wrapper").on("click",function(){
-    $("#toolTipsPjt").css("display","none");
-    $("#myMenuBox").css("display","none");
-  })
-
-
+  });
   //page-topボタン
-$(function() {
+  $(function() {
     var topBtn = $('#page-top_jq');
     topBtn.hide();
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            topBtn.fadeIn();
-            $(".tab_wrapper").addClass('tabNavigation_fixed');
-        } else {
-            topBtn.fadeOut();
-            $(".tab_wrapper").removeClass('tabNavigation_fixed');
-        }
+      if ($(this).scrollTop() > 100) {
+        topBtn.fadeIn();
+        $(".tab_wrapper").addClass('tabNavigation_fixed');
+      } else {
+        topBtn.fadeOut();
+        $(".tab_wrapper").removeClass('tabNavigation_fixed');
+      }
     });
     topBtn.click(function () {
-        $('body,  html').animate({
-            scrollTop: 0
-        },   500);
-        return false;
+      $('body,  html').animate({
+        scrollTop: 0
+      }, 500);
+      return false;
     });
-});
-
-
-// スライダー
-$(document).ready(function () {
-  //initialize swiper when document ready
-        var swiper = new Swiper('.swiper-container', {
-          speed: 400,
-          loop: true,
-          autoplay: {
-            delay: 7500,
-            disableOnInteraction: false,
-          },
-          pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
-          }
+  });
+  // スライダー
+  $(document).ready(function () {
+    //initialize swiper when document ready
+    var swiper = new Swiper('.swiper-container', {
+      speed: 400,
+      loop: true,
+      autoplay: {
+        delay: 7500,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      }
+    });
   });
 });
-
