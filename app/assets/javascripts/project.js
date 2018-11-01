1ad1eb42a40e17f104f7a832c0351387e54d069a
masuyu <masujazzcat@gmail.com>
2018-11-01 11:37:56 +0900

    Uglifierエラー対策
    app/assets/javascripts/project.js

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -26,42 +26,43 @@ $('.modal').modaal();
 
 
   //page-topボタン
-$(function() {
-    var topBtn = $('#page-top_jq');
-    topBtn.hide();
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            topBtn.fadeIn();
-            $(".tab_wrapper").addClass('tabNavigation_fixed');
-        } else {
-            topBtn.fadeOut();
-            $(".tab_wrapper").removeClass('tabNavigation_fixed');
-        }
-    });
-    topBtn.click(function () {
-        $('body,  html').animate({
-            scrollTop: 0
-        },   500);
-        return false;
-    });
-});
-
-
-// スライダー
-$(document).ready(function () {
-  //initialize swiper when document ready
-        var swiper = new Swiper('.swiper-container', {
-          speed: 400,
-          loop: true,
-          autoplay: {
-            delay: 7500,
-            disableOnInteraction: false,
-          },
-          pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
+  $(function() {
+      var topBtn = $('#page-top_jq');
+      topBtn.hide();
+      $(window).scroll(function () {
+          if ($(this).scrollTop() > 100) {
+              topBtn.fadeIn();
+              $(".tab_wrapper").addClass('tabNavigation_fixed');
+          } else {
+              topBtn.fadeOut();
+              $(".tab_wrapper").removeClass('tabNavigation_fixed');
           }
+      });
+      topBtn.click(function () {
+          $('body,  html').animate({
+              scrollTop: 0
+          },   500);
+          return false;
+      });
+  });
+
+
+  // スライダー
+  $(document).ready(function () {
+    //initialize swiper when document ready
+          var swiper = new Swiper('.swiper-container', {
+            speed: 400,
+            loop: true,
+            autoplay: {
+              delay: 7500,
+              disableOnInteraction: false,
+            },
+            pagination: {
+              el: '.swiper-pagination',
+              type: 'bullets',
+              clickable: true,
+            }
+    });
   });
 });
 
